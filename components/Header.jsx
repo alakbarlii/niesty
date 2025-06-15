8d9e1ebf298df29692d04aca753a23ec8555e03d
alakbarlii <alakbarlii002@gmail.com>
2025-06-15 22:07:13 +0400

    first wave

---
+++ components/Header.jsx
@@ -0,0 +1,23 @@
+'use client';
+
+export default function Header() {
+  return (
+    
+    <header className="w-full bg-[#08142f] py-5 px-5 sm:px-6 md:px-12 lg:px-20 flex justify-between items-center">
+    
+      {/* Logo + Brand Name */}
+      <div className="flex items-center gap-0">
+        <img src="/niesty_header.png" alt="Niesty Logo" className="h-[50px] w-auto" />
+        <span className="text-white font-semibold text-[22px]">Niesty.com</span>
+      </div>
+
+      {/* Auth Buttons */}
+      <div className="flex gap-5">
+      <button className="w-[100px] h-[45px] border-1 border-[#FFD700] text-[#FFD700] text-xl rounded-lg hover:bg-[#FFD700] hover:text-black transition">Login</button>
+        <button className="bg-yellow-400 text-black font-bold py-2 px-4 rounded-lg hover:bg-yellow-300 transition">
+          Join Now
+        </button>
+      </div>
+    </header>
+  );
+}
---
+++ components/Hero.jsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+
+export default function Hero() {
+  return (
+    <section className="bg-black text-white py-16 px-4 sm:px-6 md:px-12 mt-[60px] lg:px-20 text-center font-normal">
+      <h1 className="text-[64.8px] font-bold leading-tight">
+        SKIP THE GUESSWORK!
+      </h1>
+
+      <p className="text-[20px] font-semibold mt-[14.5px] opacity-70">
+        We match brands/businesses with perfect-fit creators.
+      </p>
+
+      <div className="relative inline-block mt-10">
+        <button
+          className="relative z-10 w-[230px] h-[50px] bg-[#FFD700] text-black font-bold text-xl rounded-lg hover:opacity-100 transition"
+        >
+          JOIN THE WAITLIST
+        </button>
+        <div className="absolute inset-0 z-0 blur-2xl bg-white opacity-50 rounded-lg pointer-events-none"></div>
+      </div>
+
+      <p className=" text-[14px] mt-[4px] font-normal opacity-70">
+        Limited slots/week
+      </p>
+    </section>
+  );
+}

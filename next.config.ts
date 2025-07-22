d483eed295f943862543a634649d4f81b42b1c5b
alakbarlii <alakbarlii002@gmail.com>
2025-07-22 16:50:03 +0400

    dfgh

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["kjoiummoobdqrsfkoqva.supabase.co"], 
+  },
 };
 
 export default nextConfig;

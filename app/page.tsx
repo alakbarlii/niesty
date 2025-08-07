99e9527c360d8eea540ecb2f0425b2bb4f9f2610
alakbarlii <alakbarlii002@gmail.com>
2025-08-07 17:14:02 +0400

    update to error fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import About from '../components/About'
-import FlowCTA from '../components/FlowCTA'
-import Footer from '../components/Footer'
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import FlowCTA from '../components/FlowCTA';
+import Footer from '../components/Footer';
 
 export default function Home() {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) return null;
+
   return (
     <main className="flex justify-center bg-black text-white w-screen overflow-x-hidden">
       <div className="w-full max-w-[1440px] flex flex-col min-h-screen">
@@ -15,5 +27,5 @@ export default function Home() {
         <Footer />
       </div>
     </main>
-  )
+  );
 }

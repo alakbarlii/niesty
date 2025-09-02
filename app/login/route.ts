069d2f0b8184097e60cc8afa2ca0238971119828
alakbarlii <alakbarlii002@gmail.com>
2025-09-03 02:53:12 +0400

    DFG

--- app/login/route.ts
+++
@@ -1,8 +0,0 @@
-// app/login/route.ts
-import { NextResponse } from 'next/server'
-
-export const HEAD = () =>
-  new NextResponse(null, {
-    status: 200,
-    headers: { 'Cache-Control': 'no-store, max-age=0' },
-  })
